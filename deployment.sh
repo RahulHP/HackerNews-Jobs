96f938f1f3ba933b869c785f2ab8f82c4f7f93cc
RahulHP <rahul.hphatak@gmail.com>
2019-10-12 17:14:25 -0500

    Moving around files

--- deployment.sh
+++ deployment.sh
@@ -1,13 +1,14 @@
-sudo cp backend.service /etc/systemd/system
+sudo cp nginx_setup/backend.service /etc/systemd/system
 sudo systemctl start backend
 sudo systemctl enable backend
-sudo cp backend /etc/nginx/sites-available/
+sudo cp nginx_setup/backend /etc/nginx/sites-available/
 sudo rm /etc/nginx/sites-enabled/backend
 sudo ln -s /etc/nginx/sites-available/backend /etc/nginx/sites-enabled
-sudo cp app.service /etc/systemd/system
+sudo cp nginx_setup/app.service /etc/systemd/system
 sudo systemctl start app
 sudo systemctl enable app
-sudo cp app /etc/nginx/sites-available/
+sudo cp nginx_setup/app /etc/nginx/sites-available/
+sudo rm /etc/nginx/sites-enabled/app
 sudo ln -s /etc/nginx/sites-available/app /etc/nginx/sites-enabled
 
 sudo systemctl restart nginx
